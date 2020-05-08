28ad4222ec248625635609be52db0e76e6a8f069
IsaDuraku <isaduraku3@gmail.com>
2020-05-08 22:15:21 +0200

    deka krejt

--- jss.js
+++ jss.js
@@ -1,5 +1,15 @@
-function ModifyDom(){
-var createTitle=
+function ModifyDom() {
+  var createTitle = document.createElement("h1");
+  var nodeText = document.createTextNode("Ky eshte nje titukk");
+  createTitle.appendChild(nodeText);
+  document.getElementsByTagName("body")[0].appendChild(createTitle);
 
+  var titulli = document.getElementsByTagName("h1")[0];
 
+  titulli.classList.add("titulli");
 }
+
+function myFunction() {
+  document.getElementById("forma").innerHTML = "forma";
+}
+/*O prof sjam ka muj me gjet ngoogle qeto me bo, po mi qet do funksione tjera, ose ose sjam ka di sakt me lyp*/
